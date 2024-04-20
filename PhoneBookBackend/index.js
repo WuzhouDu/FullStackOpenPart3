30c71bd959dff6cf888f71de50b918ad0df123e9
WuzhouDu <duwuz@outlook.com>
2024-04-20 18:30:07 +0800

    finish 3.2

--- PhoneBookBackend/index.js
+++ PhoneBookBackend/index.js
@@ -26,17 +26,29 @@ const persons = [
 ]
 
 app.get('/api/persons', (req, res) => {
-    // console.log(req.headers);
     res.json(persons);
-})
+});
 
 app.get('/info', (req, res) => {
-    // console.log(req.headers);
     res.status(200).send(`<div><p>Phonebook has info for ${persons.length} people </p> <p>${Date()}</p> </div>`);
-})
+});
+
+app.get('/api/persons/:id', (req, res) => {
+    const searchedId = Number(req.params.id);
+    const searchedPerson = persons.find(each => {
+        return each.id === searchedId;
+    });
+
+    if (searchedPerson) {
+        res.json(searchedPerson);
+    }
+    else {
+        res.status(404).send("Sorry, not found");
+    }
+});
 
 const PORT = 3002;
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`listen on port ${PORT}`);
 });
